7ea7cc6344e5af10781926b606b62053a8b5f8c4
Lamskidoski <lam.ky@hotmail.com>
2024-10-25 01:15:29 +0700

    klar med analog klockan och menu på den

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,9 @@
-// import React, { useState } from 'react';
-// import LoadingScreen from './components/LoadingScreen';
-// import SetTimer from './components/SetTimer';
-// import AnalogTimer from './components/AnalogTimer';  // Analog timer komponent
-// import DigitalTimer from './components/DigitalTimer';  // Digital timer komponent
-// import AlarmView from './components/AlarmView';
-// import BreakView from './components/BreakView';  // Komponent för pausen
-// import Menu from './components/Menu';  // Komponent för menyn
-
-// const App = () => {
-//   const [view, setView] = useState('loading');  // Hanterar olika vyer
-//   const [timerType, setTimerType] = useState('analog');  // Typ av timer (analog eller digital)
-//   const [minutes, setMinutes] = useState(10);  // Standardtid för timer
-
-//   const startTimer = (minutes, intervals, pause) => {
-//     setMinutes(minutes);
-//     setView('timer');  // Gå till timer-vyn när start-knappen klickas
-//   };
-
-//   const cancelTimer = () => {
-//     setView('set-timer');  // Gå tillbaka till timerinställningarna om användaren avbryter timern
-//   };
-
-//   const timerEnd = () => {
-//     setView('alarm');  // När timern tar slut, visa alarmet
-//   };
-
-//   const handleMenuSelect = (type) => {
-//     setTimerType(type);  // Välj mellan analog, digital, eller textbaserad timer
-//     setView('set-timer');  // Efter menyn, gå till set-timer vyn
-//   };
-
-//   return (
-//     <div>
-//       {/* Laddningsskärm */}
-//       {view === 'loading' && <LoadingScreen onLogoClick={() => setView('menu')} />}
-
-//       {/* Meny */}
-//       {view === 'menu' && <Menu onSelect={handleMenuSelect} />}
-
-//       {/* Timerinställningsvyn */}
-//       {view === 'set-timer' && <SetTimer onStartTimer={startTimer} onMenuClick={() => setView('menu')} />}
-
-//       {/* Timer (analog eller digital) */}
-//       {view === 'timer' && (
-//         timerType === 'analog' ? (
-//           <AnalogTimer minutes={minutes} onTimerEnd={timerEnd} onCancel={cancelTimer} />
-//         ) : (
-//           <DigitalTimer minutes={minutes} onTimerEnd={timerEnd} onCancel={cancelTimer} />
-//         )
-//       )}
-
-//       {/* Pausvy */}
-//       {view === 'break' && <BreakView onSkip={() => setView('timer')} />}
-
-//       {/* Alarmvy */}
-//       {view === 'alarm' && <AlarmView onReset={() => setView('set-timer')} />}
-//     </div>
-//   );
-// };
-
-// export default App;
-
 import React, { useState } from 'react';
 import LoadingScreen from './components/LoadingScreen';
 import SetTimer from './components/SetTimer';
 import AnalogTimer from './components/AnalogTimer';  // Analog timer komponent
 import DigitalTimer from './components/DigitalTimer';  // Digital timer komponent
 import AlarmView from './components/AlarmView';
-import BreakView from './components/BreakView';  // Komponent för pausen
 import Menu from './components/Menu';  // Komponent för menyn
 
 const App = () => {
@@ -75,7 +11,7 @@ const App = () => {
   const [timerType, setTimerType] = useState('analog');  // Typ av timer (analog eller digital)
   const [minutes, setMinutes] = useState(10);  // Standardtid för timer
 
-  const startTimer = (minutes, intervals, pause) => {
+  const startTimer = (minutes) => {
     setMinutes(minutes);
     setView('timer');  // Gå till timer-vyn när start-knappen klickas
   };
@@ -109,33 +45,17 @@ const App = () => {
       {view === 'set-timer' && <SetTimer onStartTimer={startTimer} onMenuClick={() => setView('menu')} />}
 
       {/* Timer (analog eller digital) */}
-      {/* {view === 'timer' && (
+      {view === 'timer' && (
         timerType === 'analog' ? (
-          <AnalogTimer minutes={minutes} onTimerEnd={timerEnd} onCancel={cancelTimer} />
+          <AnalogTimer minutes={minutes} onTimerEnd={timerEnd} onCancel={cancelTimer} onMenuClick={() => setView('menu')} />  
         ) : (
           <DigitalTimer minutes={minutes} onTimerEnd={timerEnd} onCancel={cancelTimer} />
         )
-      )} */}
+      )}
       
-      {view === 'timer' && (
-  timerType === 'analog' ? (
-    <AnalogTimer minutes={minutes} onTimerEnd={timerEnd} onCancel={cancelTimer} />  
-  ) : (
-    <DigitalTimer minutes={minutes} onTimerEnd={timerEnd} onCancel={cancelTimer} />
-  )
-)}
-
-
-      
-
-      {/* Pausvy */}
-      {view === 'break' && <BreakView onSkip={() => setView('timer')} />}
-
       {/* Alarmvy */}
       {view === 'alarm' && <AlarmView onReset={handleReset} />}  {/* Lägg till onReset-funktion */}
     </div>
-
-    
   );
 };
 
--- src/components/AnalogTimer.jsx
+++ src/components/AnalogTimer.jsx
@@ -1,27 +1,38 @@
+
 import React, { useEffect, useState } from 'react';
 import '../styles/AnalogTimer.css';
 
-const AnalogTimer = ({ minutes, onCancel }) => {
-  const [time, setTime] = useState(new Date());
+const AnalogTimer = ({ minutes, onCancel, onTimerEnd, onMenuClick }) => {
+  const [timeLeft, setTimeLeft] = useState(minutes * 60); // Startvärde i sekunder
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setTime(new Date());
-    }, 1000);
+    if (timeLeft > 0) {
+      const interval = setInterval(() => {
+        setTimeLeft((prevTime) => prevTime - 1);
+      }, 1000);
 
-    return () => clearInterval(interval);
-  }, []);
+      return () => clearInterval(interval);
+    } else if (timeLeft === 0) {
+      onTimerEnd(); // Anropa funktion när timern når noll
+    }
+  }, [timeLeft, onTimerEnd]);
 
-  const secondsDegrees = (time.getSeconds() / 60) * 360;
-  const minutesDegrees = (time.getMinutes() / 60) * 360 - 108;
+  // Beräkna vinklar för minut- och sekundvisarna
+  const seconds = timeLeft % 60;
+  const minutesRemaining = Math.floor(timeLeft / 60);
+  const secondsDegrees = -(seconds / 60) * 360;
+  const minutesDegrees = (minutesRemaining / 60) * 360 - (minutesRemaining * 6.1);
 
   return (
     <div className="timer">
+      {/* Lägg till SVG för menyikonen */}
+      <img src="/src/assets/menu.svg" alt="Menu" className="menu-icon" onClick={onMenuClick} />
+      
       <img src="/src/assets/clockface.png" alt="clock-face" className="clock-face" />
       <div className="hand minute-hand" style={{ transform: `rotate(${minutesDegrees}deg)` }} />
       <div className="hand second-hand" style={{ transform: `rotate(${secondsDegrees}deg)` }} />
 
-      <button className="abort-button" onClick={onCancel}>ABORT TIMER</button>  {/* Använder onCancel */}
+      <button className="abort-button" onClick={onCancel}>ABORT TIMER</button>
     </div>
   );
 };
